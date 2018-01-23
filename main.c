d91b3244a22ff0a34158cfb49c81d439d262132a
Long Wang <longwang.astro@gmail.com>
2018-01-23 17:17:01 +0100

    Update due to new version of Nbody6++

--- main.c
+++ main.c
@@ -4917,13 +4917,13 @@ int output5(char *output, int N, int NNBMAX, double RS0, double dtadj, double dt
 	
 	//write to .PAR file	
 	fprintf(PAR,"1 5000000.0 5000000.0 40 40 0\n");
-	fprintf(PAR,"%i 1 10 %i %i 1\n",N,seed,NNBMAX);
-	fprintf(PAR,"0.02 0.02 %.8f %.8f %.8f %.8f 1.0E-03 %.8f %.8f\n",RS0,dtadj,dtout,tcrit,rvir,mmean);
+	fprintf(PAR,"%i 1 10 %i %i 1 10\n",N,seed,NNBMAX);
+	fprintf(PAR,"0.02 0.02 %.8f %.8f %.8f %.8f 1.0E-04 %.8f %.8f\n",RS0,dtadj,dtout,tcrit,rvir,mmean);
 	fprintf(PAR,"0 2 1 0 1 0 5 %i 3 2\n",(nbin>0?2:0));
 	fprintf(PAR,"0 %i 0 %i 2 %i %i 0 %i 6\n",hrplot,tf,regupdate,etaupdate,mloss);
-	fprintf(PAR,"0 6 %i 0 1 2 2 0 0 1\n", esc);
+	fprintf(PAR,"0 6 %i 0 1 2 1 0 0 1\n", esc);
 	fprintf(PAR,"1 0 3 2 1 0 0 2 0 0\n");
-	fprintf(PAR,"0 0 0 0 0 1 2 0 0 0\n");
+	fprintf(PAR,"0 0 0 0 0 2 -3 0 0 0\n");
 	fprintf(PAR,"1.0E-5 1.0E-4 0.2 1.0 1.0E-06 0.001 0.125\n");
 	fprintf(PAR,"2.350000 %.8f %.8f %i 0 %.8f %.8f %.8f\n",MMAX,mlow,nbin,Z,epoch,dtplot);
 	fprintf(PAR,"%.2f 0.0 0.0 0.00000\n",Q);
@@ -5054,7 +5054,8 @@ void help(double msort) {
 	printf("                   3= 2-dimensional EFF/Nuker template,              \n");
 	printf("                   -1= no density gradient)                          \n");
 	printf("       -W <1-12> (W0 parameter for King model)                       \n");
-	printf("       -R <value> (half-mass radius [pc], ignored for P = 3)         \n");
+	printf("       -R <value> (half-mass radius [pc], ignored for P = 3;         \n");
+	printf("                   if -1, use Marks & Kroupa (2012) Mcl-Rh relation) \n");
 	printf("       -r <value> (scale radius of EFF/Nuker template [pc])          \n");
 	printf("       -c <value> (cut-off radius of EFF/Nuker template [pc])        \n");
 	printf("       -g <value> (power-law slope(s) of EFF/Nuker template; use     \n");
